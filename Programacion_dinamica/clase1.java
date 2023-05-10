69c01c5581ab49f695939be75f3fdd96bc8c1a23
Cristian Abel <126825548+CrisBelDev@users.noreply.github.com>
2023-05-10 11:04:02 -0400

    Update clase1.java

--- Programacion_dinamica/clase1.java
+++ Programacion_dinamica/clase1.java
@@ -1,1 +1,27 @@
 
+import java.util.Arrays;
+
+public class prueba {
+	static  long mem[] = new long[1000];
+	public static void main(String[] args) {
+		for (int i = 0; i < 1000; i++)
+			mem[i] = -1;
+		int n = 11 ;
+		for (int i = 0; i<n ; i++) {
+			System.out.print(fib_ar_ab(i)+ " ");
+			
+		}
+		System.out.println();
+		System.out.println(Arrays.toString(mem));
+	}
+	public static long fib_ar_ab(int n) {
+		if (n == 0)
+			return 0;
+		if (n == 1)
+			return 1;
+		if (mem[n] != -1)
+			return mem[n];
+		mem[n] = fib_ar_ab(n - 1 ) + fib_ar_ab(n - 2);
+		return mem[n];
+	}
+}
